de6a160c14bbe42975842939ae6d78149e56c381
Anze Slosar <slosar@gmail.com>
2021-04-08 08:24:30 -0400

    added galaxy sim stuff, improved aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,7 @@
-spython() { docker run -v "$PWD":"$PWD" -w "$PWD" -it slosar/seconda python $@; }
-sipython() { docker run -v "$PWD":"$PWD" -w "$PWD" -it slosar/seconda ipython $@; }
-adstex() { docker run -v "$PWD":/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
-alias snotebook='docker run --gpus all -v `pwd`:/here -itp 9100:9100 slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook --notebook-dir=/here --ip='*' --port=9100 --no-browser --NotebookApp.token="" --allow-root"'
-alias snotebook1='docker run --gpus all -v `pwd`:/here -itp 9100:9100 slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook --notebook-dir=/here --ip='*' --port=9101 --no-browser --NotebookApp.token="" --allow-root"'
+spython() { docker run --gpus all -v "$PWD":"$PWD" -w "$PWD" -it slosar/seconda python $@; }
+sipython() { docker run --gpus all -v "$PWD":"$PWD" -w "$PWD" -it slosar/seconda ipython $@; }
+adstex() { docker run --gpus all -v "$PWD":/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
+snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run --gpus all -v "$PWD":"$PWD" -w "$PWD" -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token="" --allow-root"; }
+
+
+
