3fa69a17cdcfda183dc315cf5af4660f80a914d6
Anze Slosar <slosar@gmail.com>
2021-04-29 03:15:12 -0400

    fixed aliases ok giotto

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,15 @@
-sbash() { docker run --user $(id -u):$(id -g) --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda /usr/bin/bash $@; }
-spython() { docker run --user $(id -u):$(id -g) -e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda python $@; }
-sipython() { docker run --user $(id -u):$(id -g) =e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda ipython $@; }
-adstex() { docker run --gpus all -v /home/anze:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
-snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run -e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token="" --allow-root"; }
+if [ $HOSTNAME == "kosovel" ]; then
+    sbash() { docker run --user $(id -u):$(id -g) --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda /usr/bin/bash $@; }
+    spython() { docker run --user $(id -u):$(id -g) -e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda python $@; }
+    sipython() { docker run --user $(id -u):$(id -g) =e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -it slosar/seconda ipython $@; }
+    adstex() { docker run --gpus all -v /home/anze:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
+    snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run -e ARES --gpus all -v /home/anze:/home/anze -w /home/anze -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token="" --allow-root"; }
+else
+    sbash() { docker run --user $(id -u):$(id -g)  -v /home/anze:/home/anze -w /home/anze -it slosar/seconda /usr/bin/bash $@; }
+    spython() { docker run --user $(id -u):$(id -g) -e ARES  -v /home/anze:/home/anze -w /home/anze -it slosar/seconda python $@; }
+    sipython() { docker run --user $(id -u):$(id -g) =e ARES  -v /home/anze:/home/anze -w /home/anze -it slosar/seconda ipython $@; }
+    adstex() { docker run  -v /home/anze:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
+    snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run -e ARES  -v /home/anze:/home/anze -w /home/anze -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token="" --allow-root"; }
+fi
+
 
