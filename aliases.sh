a32332dc6785dfce1f82d63dcd3c2e622f50a3e3
Anze Slosar <slosar@gmail.com>
2021-05-13 10:06:33 -0400

    fix ads tex bug

--- aliases.sh
+++ aliases.sh
@@ -9,7 +9,7 @@ sbash() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD  -it slosar/seconda /usr/
 spython() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD -e DISPLAY=$DISPLAY  -it slosar/seconda python $@; }
 sipython() { docker run $DOCKER_ENV $DOCKER_OPTS -w $PWD -e DISPLAY=$DISPLAY -it slosar/seconda ipython $@; }
 spip() { docker run $DOCKER_ENV $DOCKER_OPTS -w $PWD -it slosar/seconda pip $@; }
-adstex() { docker run -v /home/anze:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
+adstex() { docker run -v $PWD:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
 snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run $DOCKER_OPTS  $DOCKER_ENV -w $PWD -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token='' "; }
 
 
