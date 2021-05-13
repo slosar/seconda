cb0b477c2e8ec76747f886cd736cfee73fd2ef92
Anze Slosar <slosar@gmail.com>
2021-05-13 07:44:05 -0400

    aliases fix

--- aliases.sh
+++ aliases.sh
@@ -3,11 +3,12 @@ if [ $HOSTNAME == "kosovel" ]; then
 else
     export DOCKER_OPTS=""
 fi
-export DOCKER_ENV="-e ARES -e HOME -v /home/anze:/home/anze -w $PWD --user $(id -u):$(id -g)"
+export DOCKER_ENV="-e ARES -e HOME -v /home/anze:/home/anze -v /tmp/.X11-unix:/tmp/.X11-unix --user $(id -u):$(id -g)"
 
-sbash() { docker run  $DOCKER_ENV $DOCKER_OPTS  -it slosar/seconda /usr/bin/bash $@; }
-spython() { docker run  $DOCKER_ENV $DOCKER_OPTS -it slosar/seconda python $@; }
-sipython() { docker run $DOCKER_ENV $DOCKER_OPTS -it slosar/seconda ipython $@; }
+sbash() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD  -it slosar/seconda /usr/bin/bash $@; }
+spython() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD -e DISPLAY=$DISPLAY  -it slosar/seconda python $@; }
+sipython() { docker run $DOCKER_ENV $DOCKER_OPTS -w $PWD -e DISPLAY=$DISPLAY -it slosar/seconda ipython $@; }
+spip() { docker run $DOCKER_ENV $DOCKER_OPTS -w $PWD -it slosar/seconda pip $@; }
 adstex() { docker run -v /home/anze:/here -e ADS_API_TOKEN -it slosar/seconda adstex /here/"$1";}
 snotebook() { port=$1; [ -z "$port" ] && port=9100;  docker run $DOCKER_OPTS  $DOCKER_ENV -w $PWD -itp $port:$port slosar/seconda /bin/bash -c "/opt/conda/bin/jupyter notebook  --ip='*' --port=$port --no-browser --NotebookApp.token='' "; }
 
