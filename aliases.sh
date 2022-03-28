a27e155c27299316551e52c4992bcb7b584aa023
Anze Slosar <slosar@gmail.com>
2022-03-28 08:30:48 -0400

    updates

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,7 @@ else
     export DOCKER_OPTS=""
 fi
 export DOCKER_ENV="-e ARES -e HOME -e PYTHONPATH -v /home/anze:/home/anze -v /tmp/.X11-unix:/tmp/.X11-unix --user $(id -u):$(id -g)"
+#-v /home/anze/work/lusee/lunarsky_data:/opt/conda/lib/python3.8/site-packages/lunarsky/data 
 
 sbash() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD  -it slosar/seconda /usr/bin/bash $@; }
 spython() { docker run  $DOCKER_ENV $DOCKER_OPTS -w $PWD -e DISPLAY=$DISPLAY  -it slosar/seconda python $@; }
